5e2a333b5eae8ae4f22678f66a7b6c02c152a3cf
DavyLss <davy.lassechere@protonmail.com>
2022-05-09 16:29:53 +0000

    Changes
    Co-authored-by: DavyLss <46714675+DavyLss@users.noreply.github.com>

---
+++ src/lib/proxmox/proxy.functions.ts
@@ -0,0 +1,51 @@
+import { createServerFn } from "@tanstack/react-start";
+import { z } from "zod";
+
+const proxmoxTicketSchema = z.object({
+  baseUrl: z.string().url(),
+  username: z.string(),
+  ticket: z.string(),
+  CSRFPreventionToken: z.string(),
+  obtainedAt: z.number(),
+});
+
+const proxyInputSchema = z.object({
+  ticket: proxmoxTicketSchema,
+  path: z.string().startsWith("/"),
+  method: z.enum(["POST", "PUT", "DELETE"]),
+  body: z.record(z.unknown()).optional(),
+});
+
+export const proxmoxApiProxy = createServerFn({ method: "POST" })
+  .inputValidator((data) => proxyInputSchema.parse(data))
+  .handler(async ({ data }) => {
+    const headers: Record<string, string> = {
+      Cookie: `PVEAuthCookie=${data.ticket.ticket}`,
+      CSRFPreventionToken: data.ticket.CSRFPreventionToken,
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+
+    let body: BodyInit | undefined;
+    if (data.body) {
+      const params = new URLSearchParams();
+      for (const [key, value] of Object.entries(data.body)) {
+        if (value === undefined || value === null) continue;
+        params.append(key, String(value));
+      }
+      body = params;
+    }
+
+    const res = await fetch(`${data.ticket.baseUrl}/api2/json${data.path}`, {
+      method: data.method,
+      headers,
+      body,
+    });
+
+    if (!res.ok) {
+      const text = await res.text().catch(() => "");
+      throw new Error(`Proxmox API ${data.method} ${data.path} → ${res.status} ${text}`);
+    }
+
+    const json = (await res.json()) as { data: unknown };
+    return json.data;
+  });
