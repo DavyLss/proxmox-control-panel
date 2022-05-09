320270ae613f1feaaea3bd335847c10f17ef94bb
DavyLss <davy.lassechere@protonmail.com>
2022-05-09 16:58:04 +0000

    Changes
    Co-authored-by: DavyLss <46714675+DavyLss@users.noreply.github.com>

--- src/lib/proxmox/console-proxy.server.ts
+++ src/lib/proxmox/console-proxy.server.ts
@@ -2,48 +2,21 @@ import { z } from "zod";
 
 type WorkerSocket = WebSocket & { accept: () => void };
 type WorkerResponse = Response & { webSocket?: WorkerSocket };
-type WebSocketResponseInit = ResponseInit & { webSocket: WorkerSocket };
-type WebSocketSendData = Parameters<WebSocket["send"]>[0];
-
-declare const WebSocketPair: {
-  new (): { 0: WorkerSocket; 1: WorkerSocket };
-};
-
-const proxmoxTicketSchema = z.object({
+const consoleQuerySchema = z.object({
   baseUrl: z.string().url(),
   username: z.string(),
   ticket: z.string(),
-  CSRFPreventionToken: z.string(),
-  obtainedAt: z.number(),
-});
-
-const consoleConnectSchema = z.object({
-  ticket: proxmoxTicketSchema,
-  guest: z.object({
-    node: z.string().regex(/^[A-Za-z0-9._-]+$/),
-    type: z.enum(["qemu", "lxc"]),
-    vmid: z.number().int().positive(),
-  }),
+  node: z.string().regex(/^[A-Za-z0-9._-]+$/),
+  type: z.enum(["qemu", "lxc"]),
+  vmid: z.coerce.number().int().positive(),
+  port: z.coerce.number().int().positive(),
+  vncticket: z.string(),
 });
 
 function normalizeBaseUrl(raw: string) {
   const url = new URL(raw);
   if (url.protocol !== "https:") throw new Error("URL Proxmox HTTPS requise.");
   return url.origin;
-}
-
-function closeSocket(socket: WebSocket | undefined) {
-  if (socket && socket.readyState !== WebSocket.CLOSED) socket.close();
-}
-
-function forward(socket: WebSocket | undefined, data: MessageEvent["data"]) {
-  if (socket?.readyState === WebSocket.OPEN) socket.send(data as WebSocketSendData);
-}
-
-function sendTerminalError(socket: WorkerSocket, message: string) {
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(`\r\n\x1b[31m${message}\x1b[0m\r\n`);
-  }
 }
 
 export function isProxmoxConsoleRequest(request: Request) {
@@ -56,71 +29,28 @@ export async function handleProxmoxConsoleRequest(request: Request): Promise<Res
     return new Response("Expected Upgrade: websocket", { status: 426 });
   }
 
-  const pair = new WebSocketPair();
-  const client = pair[0];
-  const server = pair[1];
-  let upstream: WorkerSocket | undefined;
-  let connecting = false;
-
-  server.accept();
-  server.addEventListener("message", async (event) => {
-    if (upstream) {
-      forward(upstream, event.data);
-      return;
-    }
-    if (connecting) return;
-    connecting = true;
-
-    try {
-      const input = consoleConnectSchema.parse(
-        JSON.parse(typeof event.data === "string" ? event.data : new TextDecoder().decode(event.data)),
-      );
-      const baseUrl = normalizeBaseUrl(input.ticket.baseUrl);
-      const { node, type, vmid } = input.guest;
-      const consoleKind = type === "qemu" ? "kvm" : "lxc";
-      const referer = `${baseUrl}/?console=${consoleKind}&xtermjs=1&vmid=${vmid}&node=${encodeURIComponent(node)}&cmd=`;
-      const termRes = await fetch(`${baseUrl}/api2/json/nodes/${node}/${type}/${vmid}/termproxy`, {
-        method: "POST",
+  try {
+    const url = new URL(request.url);
+    const input = consoleQuerySchema.parse(Object.fromEntries(url.searchParams));
+    const baseUrl = normalizeBaseUrl(input.baseUrl);
+    const upstream = (await fetch(
+      `${baseUrl}/api2/json/nodes/${input.node}/${input.type}/${input.vmid}/vncwebsocket?port=${input.port}&vncticket=${encodeURIComponent(input.vncticket)}`,
+      {
         headers: {
-          Cookie: `PVEAuthCookie=${input.ticket.ticket}`,
-          CSRFPreventionToken: input.ticket.CSRFPreventionToken,
-          Referer: referer,
-          "Content-Type": "application/x-www-form-urlencoded",
+          Upgrade: "websocket",
+          Cookie: `PVEAuthCookie=${input.ticket}`,
         },
-      });
+      },
+    )) as WorkerResponse;
 
-      if (!termRes.ok) throw new Error(`Proxmox API termproxy → ${termRes.status}`);
-      const termJson = (await termRes.json()) as {
-        data: { ticket: string; port: string | number; user?: string };
-      };
-      const wsRes = (await fetch(
-        `${baseUrl}/api2/json/nodes/${node}/${type}/${vmid}/vncwebsocket?port=${termJson.data.port}&vncticket=${encodeURIComponent(termJson.data.ticket)}`,
-        {
-          headers: {
-            Upgrade: "websocket",
-            Cookie: `PVEAuthCookie=${input.ticket.ticket}`,
-          },
-        },
-      )) as WorkerResponse;
-
-      if (wsRes.status !== 101 || !wsRes.webSocket) {
-        throw new Error(`Proxmox WebSocket → ${wsRes.status}`);
-      }
-
-      upstream = wsRes.webSocket;
-      upstream.accept();
-      upstream.addEventListener("message", (upstreamEvent) => forward(server, upstreamEvent.data));
-      upstream.addEventListener("close", () => closeSocket(server));
-      upstream.addEventListener("error", () => closeSocket(server));
-      upstream.send(`${termJson.data.user ?? input.ticket.username}:${termJson.data.ticket}\n`);
-    } catch (error) {
-      sendTerminalError(server, error instanceof Error ? error.message : "Erreur de connexion WebSocket.");
-      closeSocket(server);
+    if (upstream.status !== 101 || !upstream.webSocket) {
+      return new Response(`Proxmox WebSocket → ${upstream.status}`, { status: 502 });
     }
-  });
 
-  server.addEventListener("close", () => closeSocket(upstream));
-  server.addEventListener("error", () => closeSocket(upstream));
-
-  return new Response(null, { status: 101, webSocket: client } as WebSocketResponseInit);
+    return upstream;
+  } catch (error) {
+    return new Response(error instanceof Error ? error.message : "Paramètres console invalides", {
+      status: 400,
+    });
+  }
 }
